dcf7309e5a709e8a4858d639309f482cbdc011ca
Bibek Chakraborty <bibekbibek0612@gmail.com>
2023-09-19 12:47:51 +0530

    Added the express file

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+const userRouter = require("./router/userRouter");
+
+app.use(express.json());
+
+app.use("/api/user", userRouter);
+
+module.exports = app;
