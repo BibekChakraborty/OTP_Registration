98760db947c9ab05d1b6b82b2ee6cdcd1b624867
Bibek Chakraborty <bibekbibek0612@gmail.com>
2023-09-21 17:28:18 +0530

    Added the gitIgnore and modified controller file

--- controller/userController.js
+++ controller/userController.js
@@ -1,13 +1,25 @@
+require("dotenv/config");
 const bcrypt = require("bcrypt");
-
 const axios = require("axios");
+const twilio = require("twilio");
 
 const otpGenerator = require("otp-generator");
 
 const { User } = require("../model/UserModel");
 const { Otp } = require("../model/otpModel");
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
 module.exports.signUp = async (req, res) => {
+  const ip =
+    req.headers["cf-connecting-ip"] ||
+    req.headers["x-real-ip"] ||
+    req.headers["x-forwarded-for"] ||
+    req.socket.remoteAddress ||
+    " ";
+  console.log(`I.P Address is : ${ip}`);
+
   const user = await User.findOne({
     number: req.body.number,
   });
@@ -23,6 +35,19 @@ module.exports.signUp = async (req, res) => {
 
   const number = req.body.number;
   console.log(OTP, number);
+
+  const client = twilio(accountSid, authToken, {
+    lazyLoading: false,
+  });
+
+  client.messages
+    .create({
+      body: `Your OTP is ${OTP}`,
+      to: number,
+      from: process.env.ADMIN_NUMBER,
+    })
+    .then((message) => console.log(message))
+    .catch((err) => console.log(err));
 
   const otp = new Otp({ number: number, otp: OTP });
   const salt = await bcrypt.genSalt(10);
@@ -46,7 +71,7 @@ module.exports.verify = async (req, res) => {
       number: latestOtp.number,
     });
     return res.status(200).send({
-      message: "User verified successfully",
+      message: "User verified successfully.",
       data: result,
     });
   } else {
--- router/userRouter.js
+++ router/userRouter.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const { signUp, verify } = require("../controller/userController");
 
-router.route("/signup").post(signUp); // Corrected the route path
+router.route("/signup").post(signUp); 
 router.route("/signup/verify").post(verify);
 
 module.exports = router;
